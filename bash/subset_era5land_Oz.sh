714634ae509532810bc73a6420f7e728644de1e3
Sami Rifai <z3530735@monsoon.ccrc.unsw.edu.au>
2020-02-18 15:36:40 +1100

    ss era5land to oz

--- bash/subset_era5land_Oz.sh
+++ bash/subset_era5land_Oz.sh
@@ -4,16 +4,19 @@
 path="/srv/ccrc/data41/z3530735/scratch/tmp_data/ERA5-Land/"
 
 #Set output path
-out_path="/srv/ccrc/data41/z3530735/ERA5-Land/"
+out_path="/srv/ccrc/data41/z3530735/ERA5-Land/Oz/"
 
 #List files (without path name, careful doesn't return in alphabetical order)
 files=`find $path"/" -type f -name "era5-land*.nc" -printf "%f\n"`
+
+#Create out directory
+mkdir -p $out_path
 
 #Loop through files
 for F in $files
 do
  echo $F
+ cdo -f nc4c -z zip_6 sellonlatbox,112,160,-55,-10 $path/$F $out_path"Oz_"$F
 done
 
-#Create out directory
-# mkdir -p $out_path
+
